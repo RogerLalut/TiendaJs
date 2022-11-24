84122cd025d311663d64b75908cf9151887e002e
Roger Lalut <roger.lalut@hotmail.com>
2022-11-24 13:08:01 -0300

    Se agrega LocalStorage y Json

--- app.js
+++ app.js
@@ -84,69 +84,133 @@ const productos = [
     img: "../Img/P-Testament.png",
   },
   {
-    id: 4,
-    nombre: "Poleron AC/DC",
+    id: 13,
+    nombre: "Poleron Faith No More",
     precio: 20000,
     cantidad: 1,
-    img: "../Img/Ac-dc-Clasico-Poleron-gris-600x375.jpg",
+    img: "../Img/Poleron-FaithNoMore.jpg",
   },
   {
-    id: 5,
-    nombre: "Poleron G & R",
+    id: 14,
+    nombre: "Poleron Alice In Chains",
     precio: 20000,
     cantidad: 1,
-    img: "../Img/Guns-N-Roses-Poleron-rosa-600x375.jpg",
+    img: "../Img/Poleron-AliceInChains.jpg",
   },
   {
-    id: 6,
-    nombre: "Poleron Nirvana",
+    id: 15,
+    nombre: "Poleron Iron Maiden",
     precio: 20000,
     cantidad: 1,
-    img: "../Img/Nirvana-Poleron-hombre-Rojo-600x375.jpg",
+    img: "../Img/Poleron-IronMaiden.jpg",
   },
   {
-    id: 7,
-    nombre: "A KIND OF MAGIC",
+    id: 16,
+    nombre: "Poleron Rammstein",
+    precio: 20000,
+    cantidad: 1,
+    img: "../Img/Poleron-Rammstein.jpg",
+  },
+  {
+    id: 17,
+    nombre: "DISCO VINILO DOBLE  QUEEN A KIND OF MAGIC",
+    precio: 35000,
+    cantidad: 1,
+    img: "../Img/Vinillo-Queen1.jpg",
+  },
+  {
+    id: 18,
+    nombre: "DISCO VINILO QUEEN SHEER HEART ATTACK",
     precio: 30000,
     cantidad: 1,
-    img: "../Img/queen1.jpg",
+    img: "../Img/Vinillo-Queen3.jpg",
   },
   {
-    id: 8,
-    nombre: "A NIGHT AT THE OPERA",
+    id: 19,
+    nombre: "DISCO VINILO QUEEN A NIGHT AT THE OPERA",
     precio: 30000,
     cantidad: 1,
-    img: "../Img/queen2.jpg",
+    img: "../Img/Vinillo-Queen2.jpg",
   },
   {
-    id: 9,
-    nombre: "SHEER HEART ATTACK",
-    precio: 30000,
+    id: 20,
+    nombre: "Accept BALLS TO THE WALL ",
+    precio: 10990,
     cantidad: 1,
-    img: "../Img/queen3.jpg",
+    img: "../Img/CD-Accept.jpg",
   },
   {
-    id: 10,
-    nombre: "Anillo Acero Madera",
-    precio: 8000,
+    id: 21,
+    nombre: "Alice In Chains ALICE IN CHAINS",
+    precio: 11990,
     cantidad: 1,
-    img: "../Img/anillo.jpg",
+    img: "../Img/CD-AliceInChains.jpg",
   },
   {
-    id: 11,
+    id: 22,
+    nombre: "Judas Priest TURBO",
+    precio: 10990,
+    cantidad: 1,
+    img: "../Img/CD-JudasPriest.jpg",
+  },
+  {
+    id: 23,
+    nombre: "Testament DARK ROOTS OF EARTH",
+    precio: 18990,
+    cantidad: 1,
+    img: "../Img/CD-Testament.jpg",
+  },
+  {
+    id: 24,
+    nombre: "Duran Duran RIO",
+    precio: 16990,
+    cantidad: 1,
+    img: "../Img/CD-DuranDuran.jpg",
+  },
+  {
+    id: 25,
+    nombre: "David Bowie LEGACY",
+    precio: 20990,
+    cantidad: 1,
+    img: "../Img/CD-DavidBowie.jpg",
+  },
+  {
+    id: 26,
     nombre: "Brazalete Calavera",
-    precio: 10000,
+    precio: 8990,
     cantidad: 1,
-    img: "../Img/brazalete.jpg",
+    img: "../Img/ACC-BrazaleteBronce.jpg",
   },
   {
-    id: 12,
-    nombre: "Brazalete Balas",
-    precio: 10000,
+    id: 27,
+    nombre: "Pañuelo Burdeo",
+    precio: 5990,
     cantidad: 1,
-    img: "../Img/Brazalete Cuero Negro Balas.jpg",
+    img: "../Img/ACC-PanueloBurdeo.jpg",
+  },
+  {
+    id: 28,
+    nombre: "Anillo Madera",
+    precio: 3990,
+    cantidad: 1,
+    img: "../Img/ACC-Anillo.jpg",
+  },
+  {
+    id: 29,
+    nombre: "Brazalete Balas",
+    precio: 8990,
+    cantidad: 1,
+    img: "../Img/ACC-BrazaleteBalas.jpg",
+  },
+  {
+    id: 30,
+    nombre: "Cinturon Motorhead",
+    precio: 12990,
+    cantidad: 1,
+    img: "../Img/ACC-Cinturon.jpg",
   },
 ];
+
 
 let carrito = [];
 
@@ -156,6 +220,13 @@ const vaciarCarrito = document.querySelector("#vaciarCarrito");
 const precioTotal = document.querySelector("#precioTotal");
 const procesarCompra = document.querySelector("#procesarCompra");
 
+document.addEventListener('DOMContentLoaded', ()=>{
+  carrito=JSON.parse(localStorage.getItem('carrito')) || [];
+  mostrarCarrito();
+})
+
+
+
 productos.forEach((product) => {
   const { id, nombre, precio, cantidad, img } = product;
   contenedor.innerHTML += `
@@ -163,7 +234,7 @@ productos.forEach((product) => {
       <img class="card-img-top mt-2" src="${img}" alt="Card image cap">
       <div class="card-body">
           <h5 class="card-title">${nombre}</h5>
-          <p class="card-text">Precio: ${precio}</p>        
+          <p class="card-text">Precio: $${precio}</p>        
           <button onclick = "agregarProducto(${id})" class="btn btn-warning ml-2">Agregar al carrito</button>
       </div>
   </div>
@@ -174,6 +245,8 @@ vaciarCarrito.addEventListener("click", () => {
   carrito.length = [];
   mostrarCarrito();
 });
+
+
 procesarCompra.addEventListener("click", () => {
   if (carrito.length === 0) {
     Swal.fire({
@@ -193,17 +266,41 @@ procesarCompra.addEventListener("click", () => {
 });
 
 function agregarProducto(id) {
-  const item = carrito.some((product) => product.id === id);
-
+  const item = carrito.some(product => product.id === id);
+  
   if (item) {
-    const product = carrito.map((product) => {
+    const product = carrito.map(product => {
       if (product.id === id) {
         product.cantidad++;
+        Swal.fire({
+          icon: "success",
+          title: 'Producto Agregado al carrito',
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+          },
+          showConfirmButton: false,
+          timer: 900
+        });
       }
     });
   } else {
     const item = productos.find((product) => product.id === id);
     carrito.push(item);
+    Swal.fire({
+      icon: "success",
+      title: 'Producto Agregado al carrito',
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      },
+      showConfirmButton: false,
+      timer: 900
+    });
   }
   mostrarCarrito();
 }
@@ -214,35 +311,41 @@ const mostrarCarrito = () => {
   carroCompras.innerHTML = "";
 
   carrito.forEach((product) => {
-    const { id, nombre, precio, img } = product;
+    const { id, nombre, precio, cantidad, img } = product;
+    
     carroCompras.innerHTML += `
-              <div class = "contenedor-carrito">
-              <div>
-                  <img class="img-fluid img-carrito" src="${img}"/>
-              </div>
-              <div>
-                  <p>${nombre}</p>
-                  <p>precio: ${precio}</p>
-                  <button onclick = "eliminarProducto(${id})"class="btn btn-danger">Eliminar Producto</button>
-              </div>
-              </div>
+    <div class = "contenedor-carrito">
+    <div>
+    <img class="img-fluid img-carrito" src="${img}"/>
+    </div>
+    <div>
+    <p>${nombre}</p>
+    <p>precio: $${precio}</p>
+    <p>cantidad: ${cantidad}</p>
+    <button onclick = "eliminarProducto(${id})"class="btn btn-danger">Eliminar Producto</button>
+    </div>
+    </div>
           `;
-  });
+});
 
   if (carrito.length === 0) {
     carroCompras.innerHTML = `
           <p class="text-center text-primary">Carrito Vacío</p>`;
-  } else {
-  }
+  } 
+  
   carritoContenedor.textContent = carrito.length;
 
   precioTotal.textContent = carrito.reduce(
     (acc, product) => acc + product.cantidad * product.precio,
     0
   );
+  guardarStorage();
 };
 function eliminarProducto(id) {
   const productoId = id;
   carrito = carrito.filter((prod) => prod.id !== productoId);
   mostrarCarrito();
+}
+function guardarStorage(){
+localStorage.setItem("carrito", JSON.stringify(carrito))
 }
